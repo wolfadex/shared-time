62b6265d26bf9db7241e4643b42c4df44a304ce3
Wolfgang Schuster <wolfgang.schuster@fourwindsinteractive.com>
2018-09-21 15:44:07 -0600

    Initial commit
    Super basic shared redux like state

---
+++ src/index.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Peer from 'peerjs';
+import { createStore } from './timestore';
+import { Provider, connect } from './timestore/components';
+
+const rootEl = document.createElement('div');
+document.body.appendChild(rootEl);
+
+const p = new Peer({ key: 'lwjd5qra8257b9' });
+
+const initialState = {
+  text: [],
+};
+const store = createStore({
+  reducer: (state = initialState, { type, ...payload }) => {
+    switch (type) {
+      case 'INSERT':
+        return {
+          ...state,
+          text: [...state.text, payload.char],
+        };
+      default:
+        return state;
+    }
+  },
+  peerId: `${Date.now()}`,
+});
+
+p.on('open', (id) => {
+  console.log('Peer ID:', id);
+  store.setId(id);
+});
+
+p.on('connection', (connection) => {
+  store.addPeer(connection.peer, connection);
+  connection.on('data', (data) => {
+    console.log('connection', data);
+    // dispatch(data);
+  });
+});
+
+const mapStateToProps = ({ text }) => ({
+  text,
+});
+
+@connect(mapStateToProps)
+class Child extends Component {
+  state = {
+    char: '',
+    id: '',
+  };
+
+  render() {
+    const { text = [], dispatch } = this.props;
+    const { char, id } = this.state;
+
+    return (
+      <div>
+        Text:
+        <p>{text.join('')}</p>
+        <input
+          type="text"
+          value={char}
+          onChange={({ target: { value } }) => this.setState({ char: value })}
+          maxLength="1"
+        />
+        <button
+          type="button"
+          disabled={char.length === 0}
+          onClick={() => {
+            dispatch({
+              char,
+              type: 'INSERT',
+            });
+          }}
+        >
+          Send
+        </button>
+        <input
+          type="text"
+          value={id}
+          onChange={({ target: { value } }) => this.setState({ id: value })}
+        />
+        <button
+          onClick={() => {
+            const connection = p.connect(id);
+
+            connection.on('open', () => {
+              console.log('connection', connection);
+              store.addPeer(connection.peer, connection);
+            });
+          }}
+        >
+          Join
+        </button>
+      </div>
+    );
+  }
+}
+
+render(
+  <Provider value={store}>
+    <Child />
+  </Provider>,
+  rootEl,
+);
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = () => {
+  return {
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? null : 'cheap-module-eval-source-map',
+    entry: ['@babel/polyfill', path.join(__dirname, 'src/index.js')],
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: isProduction ? '[name].[hash].js' : '[name].js',
+    },
+    module: {
+      rules: [
+        {
+          exclude: /node_modules/,
+          test: /.js?$/,
+          use: 'babel-loader',
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Shared Time',
+        // template: path.join(__dirname, 'src/index.html'),
+      }),
+    ],
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      port: 2222,
+      historyApiFallback: true,
+    },
+  };
+};
